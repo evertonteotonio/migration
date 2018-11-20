14eee8792552c687b9db477848d4588cd0f32f54
Cesar Gimenes <crg@crg.eti.br>
2018-11-19 22:23:31 -0200

    add list files function

--- migration.go
+++ migration.go
@@ -1,7 +1,19 @@
 package migration
 
-// findFiles search for migration files and return
-// a sorted array with the full path of all found files
-func findFiles() (files []string, err error) {
+import (
+	"path/filepath"
+)
+
+// upFiles search for migration up files and return
+// a sorted array with the path of all found files
+func upFiles(dir string) (files []string, err error) {
+	files, err = filepath.Glob(filepath.Join(dir, "*.up.sql"))
+	return
+}
+
+// downFiles search for migration down files and return
+// a sorted array with the path of all found files
+func downFiles(dir string) (files []string, err error) {
+	files, err = filepath.Glob(filepath.Join(dir, "*.down.sql"))
 	return
 }
--- migration_test.go
+++ migration_test.go
@@ -5,23 +5,63 @@ import (
 	"testing"
 )
 
-func Test_findFiles(t *testing.T) {
+func Test_upFiles(t *testing.T) {
 	tests := []struct {
 		name      string
 		wantFiles []string
 		wantErr   bool
+		path      string
 	}{
-		// TODO: Add test cases.
+		{
+			name: "list files",
+			path: "fixtures",
+			wantFiles: []string{
+				"fixtures/001_name.up.sql",
+				"fixtures/002_b_name.up.sql",
+				"fixtures/003_a_name.up.sql",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotFiles, err := findFiles()
+			gotFiles, err := upFiles(tt.path)
 			if (err != nil) != tt.wantErr {
-				t.Errorf("findFiles() error = %v, wantErr %v", err, tt.wantErr)
+				t.Errorf("upFiles() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
 			if !reflect.DeepEqual(gotFiles, tt.wantFiles) {
-				t.Errorf("findFiles() = %v, want %v", gotFiles, tt.wantFiles)
+				t.Errorf("upFiles() = %v, want %v", gotFiles, tt.wantFiles)
+			}
+		})
+	}
+}
+
+func Test_downFiles(t *testing.T) {
+	tests := []struct {
+		name      string
+		wantFiles []string
+		wantErr   bool
+		path      string
+	}{
+		{
+			name: "list files",
+			path: "fixtures",
+			wantFiles: []string{
+				"fixtures/001_name.down.sql",
+				"fixtures/002_b_name.down.sql",
+				"fixtures/003_a_name.down.sql",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotFiles, err := downFiles(tt.path)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("downFiles() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(gotFiles, tt.wantFiles) {
+				t.Errorf("downFiles() = %v, want %v", gotFiles, tt.wantFiles)
 			}
 		})
 	}
