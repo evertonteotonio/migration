6c9a0e9c4b0c7f1896f36d92af58fe919a66e055
Cesar Gimenes <crg@crg.eti.br>
2018-12-09 19:14:52 -0200

    add parameters

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/crgimenes/goconfig"
+)
+
 func main() {
+	config := struct {
+		Database string `cfg:"db" crgRequired:"true" cfgHelper:""`
+		Source   string `cfg:"source" crgRequired:"true" cfgHelper:""`
+		Migrate  string `cfg:"migrate" crgRequired:"true" cfgHelper:""`
+	}{}
+
+	err := goconfig.Parse(&config)
+	if err != nil {
+		println(err)
+		return
+	}
+
+	fmt.Println(config)
 
 }
