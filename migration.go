acdaa1e70287601a68d38b339179b2da0ae0bb7b
Cesar Gimenes <crg@crg.eti.br>
2018-12-10 08:04:36 -0200

    draft up function

--- migration.go
+++ migration.go
@@ -3,6 +3,8 @@ package migration
 import (
 	"fmt"
 	"path/filepath"
+	"strconv"
+	"strings"
 )
 
 // upFiles search for migration up files and return
@@ -19,13 +21,43 @@ func downFiles(dir string) (files []string, err error) {
 	return
 }
 
-func Run(source, database, migrate string) (err error) {
+func up(source string, n int) (err error) {
+	fmt.Println("up", n)
+
 	files, err := upFiles(source)
 	if err != nil {
 		return
 	}
 	for _, f := range files {
 		fmt.Println(f)
+	}
+	return
+}
+
+func down(source string, n int) (err error) {
+	fmt.Println("down", n)
+	return
+}
+
+func Run(source, database, migrate string) (err error) {
+	m := strings.Split(migrate, " ")
+	switch m[0] {
+	case "up":
+		var n int
+		n, err = strconv.Atoi(m[1])
+		if err != nil {
+			return
+		}
+		err = up(source, n)
+	case "down":
+		var n int
+		n, err = strconv.Atoi(m[1])
+		if err != nil {
+			return
+		}
+		err = down(source, n)
+	default:
+		err = fmt.Errorf("unknown migration command")
 	}
 	return
 }
